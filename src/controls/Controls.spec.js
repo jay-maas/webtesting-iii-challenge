e86901e0a8d99dd6165fe1134502a61cb3bd4bd8
Jay Maas <jaymaas.dev@gmail.com>
2019-06-19 16:54:38 -0600

    Dashboard testing
    initializing tests for overall functionality of the app

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/react/cleanup-after-each'
 
 import Controls from './Controls'
 
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/react/cleanup-after-each'
 
 import Dashboard from './Dashboard'
 
@@ -7,6 +8,33 @@ describe('<Dashboard />', () => {
    
     it('renders without errors', () => {
         render(<Dashboard />)
+    })
+
+    it('click close gate', () => {
+        const { getByText } = render(<Dashboard />)
+        const button = getByText(/close gate/i)
+       
+        fireEvent.click(button)
+        
+        expect(getByText(/open gate/i))
+        expect(getByText(/closed/i))
+        expect(getByText('Unlocked'))
+        expect(getByText(/lock gate/i))
+    })
+
+    it('click open gate', () => {
+        const { getByText } = render(<Dashboard />)
+
+        fireEvent.click(getByText(/close gate/i))
+
+        const button = getByText(/open gate/i)
+
+        fireEvent.click(button)
+
+        expect(getByText(/close gate/i))
+        expect(getByText(/open/i))
+        expect(getByText('Unlocked'))
+        expect(getByText(/lock gate/i))
     })
 
 })
